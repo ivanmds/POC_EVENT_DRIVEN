74f95da9a5d3fff1f9caba19b082c0b534a95961
Ivan Meireles da Silva <ivan.silva@tooseguros.com.br>
2022-02-02 18:19:16 -0300

    adjusting to using docker-compose and dockerfile

--- customer/src/app.module.ts
+++ customer/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { environment } from './common/environment';
 import { KafkaBus } from './common/kafka/kafka-bus';
 import { Mapper } from './common/mappers/mapper';
@@ -8,6 +9,7 @@ import { CustomerService } from './domain/services/customer.service';
 
 @Module({
   imports: [
+    PrometheusModule.register(),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: environment.fileName() }),
   ],
   providers: [KafkaBus, Mapper, CustomerService],
