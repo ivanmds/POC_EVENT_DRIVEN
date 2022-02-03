9d0a32ad700d57215c03a54e17414b2ac9f67fd3
Ivan Meireles da Silva <ivan.silva@tooseguros.com.br>
2022-02-02 22:20:49 -0300

    configuration to save events in mongo

--- customer/src/domain/services/customer.service.ts
+++ customer/src/domain/services/customer.service.ts
@@ -1,26 +1,25 @@
 import { CustomerCreateCommand } from "src/dtos/commands/customer-create.command";
-import { KafkaBus } from "src/common/kafka/kafka-bus";
 import { Customer } from "../entities/customer.entity";
 import { ResultData } from "src/common/result";
 import { Injectable } from "@nestjs/common";
+import { CustomerEventStoreRepository } from "../repositories/customer-event-store.repository";
+import { errorMapped } from "src/common/error-mapped";
 
 @Injectable()
 export class CustomerService {
 
     constructor(
-        private bus: KafkaBus) { }
+        private eventStore: CustomerEventStoreRepository) { }
 
     async create(command: CustomerCreateCommand): Promise<ResultData<Customer>> {
 
         const customer = Customer.create(command.name, command.motherName, command.birthDate);
 
-        const kafkaResult = await this.bus.publishMessages(customer.getUncommittedEvents());
-
-        if (kafkaResult.isSuccess() == false) {
-            return ResultData.fail4(kafkaResult.getErrors());
+        const result = await this.eventStore.save(customer);
+        if(result.isFail()) {
+            return ResultData.fail3(errorMapped.saveCustomerError());
         }
 
-        customer.clearUncommittedEvents();
         return ResultData.okWithData(customer);
     }
 }
