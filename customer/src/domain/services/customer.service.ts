610194b0d56c1e9d3d300ba9ee21b7b4be7c2f79
Ivan Meireles da Silva <ivan.silva@tooseguros.com.br>
2022-02-02 22:27:49 -0300

    return publish to kafka

--- customer/src/domain/services/customer.service.ts
+++ customer/src/domain/services/customer.service.ts
@@ -4,21 +4,34 @@ import { ResultData } from "src/common/result";
 import { Injectable } from "@nestjs/common";
 import { CustomerEventStoreRepository } from "../repositories/customer-event-store.repository";
 import { errorMapped } from "src/common/error-mapped";
+import { KafkaBus } from "src/common/kafka/kafka-bus";
 
 @Injectable()
 export class CustomerService {
 
     constructor(
-        private eventStore: CustomerEventStoreRepository) { }
+        private eventStore: CustomerEventStoreRepository,
+        private bus: KafkaBus) { }
 
     async create(command: CustomerCreateCommand): Promise<ResultData<Customer>> {
 
         const customer = Customer.create(command.name, command.motherName, command.birthDate);
 
         const result = await this.eventStore.save(customer);
-        if(result.isFail()) {
+        if (result.isFail()) {
             return ResultData.fail3(errorMapped.saveCustomerError());
         }
+
+        const kafkaResult = await this.bus.publishMessages(
+            process.env.TOPIC_EXTERNAL_EVENTS,
+            customer.getUncommittedEvents()
+        );
+
+        if (kafkaResult.isFail()) {
+            return ResultData.fail4(kafkaResult.getErrors());
+        }
+
+        customer.clearUncommittedEvents();
 
         return ResultData.okWithData(customer);
     }
